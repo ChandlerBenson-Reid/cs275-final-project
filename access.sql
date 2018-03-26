71d24bf17b9dcc81d961a38880d16d7ee51808b5
chandler <cob25@drexel.edu>
2018-03-25 21:17:45 -0400

    Update access.sql with more example files

--- access.sql
+++ access.sql
@@ -4,7 +4,6 @@
 * Access
 */
 
-
 DROP TABLE Content;
 DROP TABLE Users;
 DROP TABLE Organizations;
@@ -43,9 +42,29 @@ VALUES
 INSERT INTO Users(username, user_pass, admin_status, rank, org_name)
 VALUES
 ('test1', 'test1', 1, 1, 'CS 275'),
-('test2', 'test2', 0, 1, 'CS 275');
+('test2', 'test2', 0, 1, 'CS 275'),
+('test3', 'test3', 1, 2, 'CS 275'),
+('test4', 'test4', 0, 2, 'CS 275'),
+('test5', 'test5', 1, 3, 'CS 275'),
+('test6', 'test6', 0, 3, 'CS 275'),
+('test7', 'test7', 1, 4, 'CS 275'),
+('test8', 'test8', 0, 4, 'CS 275'),
+('test9', 'test9', 1, 5, 'CS 275'),
+('test10', 'test10', 0, 5, 'CS 275');
 
 INSERT INTO Content(content_name, content_path, necessary_rank, is_archived, org_name)
 VALUES
 ('testContent1', './content/content1', 0, 0, 'CS 275'),
-('testContent2', './content/content2', 0, 0, 'CS 275');
+('testContent2', './content/content2', 0, 0, 'CS 275'),
+('testContent3', './content/content3', 1, 0, 'CS 275'),
+('testContent4', './content/content4', 1, 0, 'CS 275'),
+('testContent5', './content/content5', 1, 0, 'CS 275'),
+('testContent6', './content/content6', 2, 0, 'CS 275'),
+('testContent7', './content/content7', 2, 0, 'CS 275'),
+('testContent8', './content/content8', 3, 0, 'CS 275'),
+('testContent9', './content/content9', 3, 0, 'CS 275'),
+('testContent10', './content/content10', 3, 0, 'CS 275'),
+('testContent11', './content/content11', 4, 0, 'CS 275'),
+('testContent12', './content/content12', 4, 0, 'CS 275'),
+('testContent13', './content/content13', 5, 0, 'CS 275');
+('testContent14', './content/content14', 5, 0, 'CS 275'),
