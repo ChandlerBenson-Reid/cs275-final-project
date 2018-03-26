f0a55fc15eccef3ece87ba0bc831bd933c029a14
chandler <cob25@drexel.edu>
2018-03-25 22:03:46 -0400

    Update database content for more coherent examples

--- access.sql
+++ access.sql
@@ -36,35 +36,31 @@ CREATE TABLE Content (
 
 INSERT INTO Organizations(org_name)
 VALUES
-('CS 275'),
-('CS 260');
+('Drexel Karate Club');
+--('CS 260');
 
 INSERT INTO Users(username, user_pass, admin_status, rank, org_name)
 VALUES
-('test1', 'test1', 1, 1, 'CS 275'),
-('test2', 'test2', 0, 1, 'CS 275'),
-('test3', 'test3', 1, 2, 'CS 275'),
-('test4', 'test4', 0, 2, 'CS 275'),
-('test5', 'test5', 1, 3, 'CS 275'),
-('test6', 'test6', 0, 3, 'CS 275'),
-('test7', 'test7', 1, 4, 'CS 275'),
-('test8', 'test8', 0, 4, 'CS 275'),
-('test9', 'test9', 1, 5, 'CS 275'),
-('test10', 'test10', 0, 5, 'CS 275');
+('Sensei', 'senseipass', 1, 5, 'Drexel Karate Club'),
+('whitebelt', 'student1', 0, 1, 'Drexel Karate Club'),
+('yellowbelt', 'student2', 0, 2, 'Drexel Karate Club'),
+('orangebelt', 'student3', 0, 2, 'Drexel Karate Club'),
+('greenbelt', 'student4', 0, 3, 'Drexel Karate Club'),
+('purplebelt', 'student5', 0, 3, 'Drexel Karate Club'),
+('brownbelt', 'student6', 0, 3, 'Drexel Karate Club'),
+('redbelt', 'student7', 0, 4, 'Drexel Karate Club'),
+('blackbelt', 'student8', 0, 4, 'Drexel Karate Club');
 
 INSERT INTO Content(content_name, content_path, necessary_rank, is_archived, org_name)
 VALUES
-('testContent1', './content/content1', 0, 0, 'CS 275'),
-('testContent2', './content/content2', 0, 0, 'CS 275'),
-('testContent3', './content/content3', 1, 0, 'CS 275'),
-('testContent4', './content/content4', 1, 0, 'CS 275'),
-('testContent5', './content/content5', 1, 0, 'CS 275'),
-('testContent6', './content/content6', 2, 0, 'CS 275'),
-('testContent7', './content/content7', 2, 0, 'CS 275'),
-('testContent8', './content/content8', 3, 0, 'CS 275'),
-('testContent9', './content/content9', 3, 0, 'CS 275'),
-('testContent10', './content/content10', 3, 0, 'CS 275'),
-('testContent11', './content/content11', 4, 0, 'CS 275'),
-('testContent12', './content/content12', 4, 0, 'CS 275'),
-('testContent13', './content/content13', 5, 0, 'CS 275');
-('testContent14', './content/content14', 5, 0, 'CS 275'),
+('Class Schedule', './drexel_karate_club/class_schedule.txt', 0, 0, 'Drexel Karate Club'),
+('White Belt Forms', './drexel_karate_club/whitebelt_forms.txt', 1, 0, 'Drexel Karate Club'),
+('White Belt Test', './drexel_karate_club/whitebelt_test.txt', 1, 0, 'Drexel Karate Club'),
+('Orange Belt Forms', './drexel_karate_club/orangebelt_forms.txt', 2, 0, 'Drexel Karate Club'),
+('Orange Belt Test', './drexel_karate_club/orangebelt_test.txt', 2, 0, 'Drexel Karate Club'),
+('Purple Belt Forms', './drexel_karate_club/purblebelt_forms.txt', 3, 0, 'Drexel Karate Club'),
+('Purple Belt Test', './drexel_karate_club/purplebelt_test.txt', 3, 0, 'Drexel Karate Club'),
+('Red Belt Forms', './drexel_karate_club/redbelt_forms.txt', 4, 0, 'Drexel Karate Club'),
+('Red Belt Test', './drexel_karate_club/redbelt_test.txt', 4, 0, 'Drexel Karate Club'),
+('Black Belt Forms', './drexel_karate_club/blackbelt_forms.txt', 5, 0, 'Drexel Karate Club'),
+('Black Belt Test', './drexel_karate_club/blackbelt_test.txt', 5, 0, 'Drexel Karate Club');
